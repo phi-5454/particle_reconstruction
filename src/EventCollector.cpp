46b3aba09208313edce3b5660f6096dc5a4e1b77
younes <younes.malek.elberkennou@cern.ch>
2024-06-27 09:58:24 +0200

    Added 2d histogram drawing, fixed find_min_max

--- src/EventCollector.cpp
+++ src/EventCollector.cpp
@@ -13,13 +13,13 @@ EventCollector::EventCollector() {
 }
 
 void EventCollector::initialize_events() {
+  // std::string infile = this->filepath + "TOTEM20.root";
+  // std::string files = this->filepath + "TOTEM2*.root?#tree";
+  std::string files = "../TOTEM20.root?#tree";
 
-  std::string infile = this->filepath + "TOTEM20.root";
-  std::string files = this->filepath + "TOTEM2*.root?#tree";
-
-  TFile *h = TFile::Open(infile.c_str());
-
+  // TFile *h = TFile::Open(infile.c_str());
   TChain *chain = new TChain("hugetree");
+
   chain->Add(files.c_str());
 
   TTreeReader myReader(chain);
@@ -50,11 +50,10 @@ void EventCollector::initialize_events() {
   }
   std::cout << "Finished initializing events" << std::endl;
 
-  h->Close();
+  // h->Close();
 }
 
-template <typename F> 
-float EventCollector::find_min_max(F &&lambda) {
+template <typename F> std::tuple<float, float> EventCollector::find_min_max(F &&lambda) {
   float min = HUGE_VALF;
   float max = -HUGE_VALF;
   for (Event *&event : events) {
@@ -66,8 +65,10 @@ float EventCollector::find_min_max(F &&lambda) {
         max = value;
     }
   }
-  return min, max;
+  return std::make_tuple(min, max);
 }
+
+/// 1D Histograms
 
 template <typename F>
 TH1F *EventCollector::create_1Dhistogram(F &&lambda, int bins, float low,
@@ -86,7 +87,7 @@ TH1F *EventCollector::create_1Dhistogram(F &&lambda, int bins, float low,
 
 template <typename F>
 TH1F *EventCollector::create_1Dhistogram(F &&lambda, bool draw) {
-  float min, max = find_min_max(lambda);
+  auto [min, max] = find_min_max(lambda);
   return create_1Dhistogram(lambda, round(sqrt(2 * events.size())), min, max,
                             "A histogram for a fit", draw);
 }
@@ -102,13 +103,42 @@ TF1 *EventCollector::create_1Dhistogram_fit(F &&lambda, int bins, float low,
 
 template <typename F>
 TF1 *EventCollector::create_1Dhistogram_fit(F &&lambda, std::string distr) {
-  float min, max = find_min_max(lambda);
+  auto [min, max] = find_min_max(lambda);
   return create_1Dhistogram_fit(lambda, round(sqrt(events.size() / 2)), min,
                                 max, "A histogram for a fit", distr);
 }
 
-template <typename F>
-void EventCollector::filter_events(F &&lambda) {
+/// 2D Histograms
+
+template <typename F1, typename F2>
+TH2F *EventCollector::create_2Dhistogram(F1 &&lambda_x, F2 &&lambda_y, int bins_x, float low_x, float high_x,
+                             int bins_y, float low_y, float high_y,
+                             const std::string& title, bool draw) {
+    TH2F *hist = new TH2F("hist", title.c_str(), bins_x, low_x, high_x, bins_y, low_y, high_y);
+    for (Event *&event : events) {
+        std::vector<float> values_x = lambda_x(event);
+        std::vector<float> values_y = lambda_y(event);
+        // Ass. len(values_x) == len(values_y)
+        for (int i = 0; i < values_x.size(); ++i) {
+            hist->Fill(values_x[i], values_y[i]);
+        }
+    }
+    if (draw)
+        hist->Draw("Colz");
+    return hist;
+}
+
+
+template <typename F1, typename F2>
+TH2F *EventCollector::create_2Dhistogram(F1 &&lambda_x, F2 &&lambda_y, const std::string& title,  bool draw) {
+    auto [min_x, max_x] = find_min_max(lambda_x);
+    auto [min_y, max_y] = find_min_max(lambda_y);
+    return create_2Dhistogram(lambda_x, lambda_y, round(sqrt(2 * events.size())), min_x, max_x,
+                              round(sqrt(2 * events.size())), min_y, max_y,
+                              "A title", draw);
+}
+
+template <typename F> void EventCollector::filter_events(F &&lambda) {
   auto helper = std::vector<Event *>(events.size());
   auto it = std::copy_if(events.begin(), events.end(), helper.begin(), lambda);
   helper.resize(it - helper.begin());
@@ -137,10 +167,10 @@ void EventCollector::filter_events_distribution(F &&lambda, std::string distr,
 template <typename F>
 void EventCollector::filter_events_distribution(F &&lambda, std::string distr,
                                                 float sigmaMulti) {
-  float min, max = find_min_max(lambda);
-  filter_events_distribution(lambda, distr, sigmaMulti,
-                             round(sqrt(events.size() / 2)), min, max,
-                             "A histogram for a fit");
+    auto [min, max]= find_min_max(lambda);
+    filter_events_distribution(lambda, distr, sigmaMulti,
+                               round(sqrt((double)events.size() / 2)), min, max,
+                               "A histogram for a fit");
 }
 
 void EventCollector::filter_initial_events() {
@@ -223,26 +253,47 @@ void EventCollector::analyze(std::string filename) {
       },
       300, -1, 1.5, "Particle distance from primary vertex in z-axis",
       true);
+
+  c1->cd(4);
+    auto l1 = [](Event *event) {
+        std::vector<float> values(4);
+        for (int i = 0; i < 4; ++i) {
+            values[i] = event->get_particle(0, 0, i)->dz;
+        }
+        return values;
+    };
+    auto l2 = [](Event *event) {
+        std::vector<float> values(4);
+        for (int i = 0; i < 4; ++i) {
+            values[i] = event->get_particle(0, 0, i)->dxy;
+        }
+        return values;
+    };
+
+    TH2 *h4 = create_2Dhistogram(
+            l1, l2,
+            "Track dxy vs dx",
+            true);
+
   h1->Write();
   h2->Write();
+  h3->Write();
+  h4->Write();
   c1->SaveAs(filename.c_str());
   results->Close();
 
   std::cout << "Finished analyzing" << std::endl;
 }
 
-void EventCollector::init_masses_and_energy(float mass)
-{
-  for (Event* &event : events) for (int i = 0; i < 4; ++i) 
-  {
-    event->set_masses_and_energies(mass);
-  }
+void EventCollector::init_masses_and_energy(float mass) {
+  for (Event *&event : events)
+    for (int i = 0; i < 4; ++i) {
+      event->set_masses_and_energies(mass);
+    }
 }
 
-void EventCollector::reconstruct_particles()
-{
-  for (Event* &event : events)
-  {
+void EventCollector::reconstruct_particles() {
+  for (Event *&event : events) {
     event->reconstruct();
   }
 }
@@ -256,15 +307,24 @@ void EventCollector::analyze_reco(std::string filename) {
 
   TH1 *h2 = create_1Dhistogram(
       [](Event *event) {
+        /*        for (auto &a : event->particles) {
+                  for (auto &b : a) {
+                    for (auto &c : b) {
+                      std::cout << "a ";
+                    }
+                  std::cout << "\t";
+                  }
+                  std::cout << std::endl;
+                }*/
         std::vector<float> values(4);
         for (int i = 0; i < 2; ++i) {
           for (int j = 0; j < 2; ++j) {
-            values[2 * i + j] = event->get_particle(1, i, j)->mass;
+            //values[2 * i + j] = event->get_proton(0)->Thx;
           }
         }
         return values;
       },
-      200, 0.25, 1.3, "Mass of recreated particles, assumed kaons",
+      200, 0.9, 1.3, "Mass of recreated particles, assumed kaons",
       true);
   h2->Write();
   c1->SaveAs(filename.c_str());
--- src/EventCollector.h
+++ src/EventCollector.h
@@ -1,75 +1,81 @@
 #ifndef EVENTCOLLECTOR_H
 #define EVENTCOLLECTOR_H
 
-#include <vector>
-#include <string>
 #include "Event.h"
 #include "TFitResultPtr.h"
 #include "TH1.h"
+#include "TH2.h"
+#include <string>
+#include <vector>
+
 
 /**
  * @brief Handles all the inspected events.
- * 
+ *
  */
-class EventCollector
-{
+class EventCollector {
 public:
-    std::string filepath = "/eos/cms/store/group/phys_diffraction/CMSTotemLowPU2018/ntuples/data/";
-                            /*/eos/user/y/yelberke/TOTEM_2018_ADDEDVARS_OUT*/
-    std::string results = "/afs/cern.ch/user/p/ptuomola/private/particle_reconstruction_results.root";
-    std::vector<Event*> events;
+    std::string filepath =
+            "/eos/cms/store/group/phys_diffraction/CMSTotemLowPU2018/ntuples/data/";
+    /*/eos/user/y/yelberke/TOTEM_2018_ADDEDVARS_OUT*/
+    // std::string results =
+    // "/afs/cern.ch/user/p/ptuomola/private/particle_reconstruction_results.root";
+    std::string results = "../res.root";
+    std::vector<Event *> events;
 
     /**
      * @brief Construct a new Event Collector object
-     * 
+     *
      */
     EventCollector();
 
     /**
      * @brief Creates and assings all the events (and thus particles)
-     * 
+     *
      */
     void initialize_events();
 
     /**
-     * @brief Finds and returns the lowest and highest values of given lambda function
-     * 
+     * @brief Finds and returns the lowest and highest values of given lambda
+     * function
+     *
      * @tparam F A lambda function
      * @param lambda Property to be measured
      * @return float The lowest and highest values of the property
      */
-    template <typename F>
-    float find_min_max(F&& lambda);
+    template <typename F> std::tuple<float, float> find_min_max(F &&lambda);
 
     /**
      * @brief Create a 1D histogram
-     * 
+     *
      * @tparam F A lambda function
-     * @param lambda Property to be fitted as a lambda for event. Needs to return a vector of floats.
+     * @param lambda Property to be fitted as a lambda for event. Needs to return
+     * a vector of floats.
      * @param bins Amount of bins
      * @param low Lower limit of bins
      * @param high Upper limit of bins
      * @param title Title of the histogram
      * @param draw Whether or not to draw the histogram
-     * @return TH1F* 
+     * @return TH1F*
      */
     template <typename F>
-    TH1F* create_1Dhistogram(F&& lambda, int bins, float low, float high, std::string title, bool draw);
+    TH1F *create_1Dhistogram(F &&lambda, int bins, float low, float high,
+                             std::string title, bool draw);
 
     /**
      * @brief Create a 1D histogram for fitting
-     * 
+     *
      * @tparam F A lambda function
-     * @param lambda Property to be fitted as a lambda for event. Needs to return a vector of floats.
+     * @param lambda Property to be fitted as a lambda for event. Needs to return
+     * a vector of floats.
      * @param draw Whether or not to draw the histogram
-     * @return TH1F* 
+     * @return TH1F*
      */
-    template <typename F>
-    TH1F* create_1Dhistogram(F&& lambda, bool draw);
+    template <typename F> TH1F *create_1Dhistogram(F &&lambda, bool draw);
 
     /**
      * @brief Create a histogram with a fit
-     * 
+     *
      * @tparam F A lambda function
      * @param lambda Property to be fitted as a lambda
      * @param bins Amount of bins
@@ -77,93 +83,131 @@ public:
      * @param high Upper limit of bins
      * @param title Title of the histogram
      * @param distr The distribution used in the fit
-     * @return TF1* The fitted function 
+     * @return TF1* The fitted function
      */
     template <typename F>
-    TF1* create_1Dhistogram_fit(F&& lambda, int bins, float low, float high, std::string title, std::string distr);
+    TF1 *create_1Dhistogram_fit(F &&lambda, int bins, float low, float high,
+                                std::string title, std::string distr);
 
     /**
      * @brief Create a histogram with a fit
-     * 
+     *
      * @tparam F A lambda function
      * @param lambda Property to be fitted as a lambda
      * @param distr The distribution used in the fit
-     * @return TF1* The fitted function 
+     * @return TF1* The fitted function
      */
     template <typename F>
-    TF1* create_1Dhistogram_fit(F&& lambda, std::string distr);
+    TF1 *create_1Dhistogram_fit(F &&lambda, std::string distr);
+
+
+    /**
+     * @brief Create a 2D histogram. MAKE SURE TO PLOT COMPATIBLE VARIABLES
+     *
+     * @tparam F lambda function type
+     * @param lambda_x Property to be fitted as a lambda for event on x axis. Needs to return
+     * a vector of floats.
+     * @param lambda_x Property to be fitted as a lambda for event on y axis. Needs to return a vector of floats
+     * @param bins Amount of bins
+     * @param low Lower limit of bins
+     * @param high Upper limit of bins
+     * @param title Title of the histogram
+     * @param draw Whether or not to draw the histogram
+     * @return TH2F*
+     */
+    template <typename F1, typename F2>
+    TH2F *create_2Dhistogram(F1 &&lambda_x, F2 &&lambda_y, int bins_x, float low_x, float high_x,
+                             int bins_y, float low_y, float high_y,
+                             const std::string& title, bool draw);
+
+
+    /**
+     * @brief Create a 1D histogram for fitting
+     *
+     * @tparam F A lambda function
+     * @param lambda Property to be fitted as a lambda for event. Needs to return
+     * a vector of floats.
+     * @param draw Whether or not to draw the histogram
+     * @return TH1F*
+     */
+    template <typename F1, typename F2> TH2F *create_2Dhistogram(F1 &&lambda_x, F2 &&lambda_y, const std::string& title, bool draw);
 
     /**
      * @brief Filters the events based on the given lambda function
-     * 
+     *
      * @tparam F A lambda function
      * @param lambda The filter function
      */
-    template <typename F>
-    void filter_events(F&& lambda);
+    template <typename F> void filter_events(F &&lambda);
 
     /**
      * @brief Filters the events based on the distribution
-     * 
+     *
      * @tparam F A Lambda function
-     * @param lambda The property to be fitted 
+     * @param lambda The property to be fitted
      * @param distr The distribution function
-     * @param sigma The selected area around mean value, +- sigmas. Eg. 3 = -+ 3 sigmas.
+     * @param sigma The selected area around mean value, +- sigmas. Eg. 3 = -+ 3
+     * sigmas.
      * @param bins Amount of bins
      * @param low Lower limit of bins
      * @param high Upper limit of bins
      * @param title Title of the histogram
      */
     template <typename F>
-    void filter_events_distribution(F&& lambda, std::string distr, float sigma, int bins, float low, float high, std::string title);
+    void filter_events_distribution(F &&lambda, std::string distr, float sigma,
+                                    int bins, float low, float high,
+                                    std::string title);
 
     /**
      * @brief Filters the events based on the distribution
-     * 
+     *
      * @tparam F A Lambda function
-     * @param lambda The property to be fitted in the histogram that is then fitted
+     * @param lambda The property to be fitted in the histogram that is then
+     * fitted
      * @param distr The distribution function
-     * @param sigma The selected area around mean value, +- sigmas. Eg. 3 = -+ 3 sigmas.
+     * @param sigma The selected area around mean value, +- sigmas. Eg. 3 = -+ 3
+     * sigmas.
      */
     template <typename F>
-    void filter_events_distribution(F&& lambda, std::string distr, float sigma);
+    void filter_events_distribution(F &&lambda, std::string distr, float sigma);
 
     /**
      * @brief Filters the inial events to fit the provided criteria
-     * 
+     *
      */
     void filter_initial_events();
 
     /**
      * @brief Analyzes the data
-     * 
+     *
      * @param filename Name of the file (pdf) where the histograms are drawn
      */
     void analyze(std::string filename);
 
     /**
      * @brief Analyzes the data of recreated particles
-     * 
+     *
      * @param filename Name of the file (pdf) where the histograms are drawn
      */
     void analyze_reco(std::string filename);
 
     /**
      * @brief Filters the data
-     * 
+     *
      */
     void filter();
 
     /**
-     * @brief Initializes the masses of the particles to given value and energy based on the mass
-     * 
+     * @brief Initializes the masses of the particles to given value and energy
+     * based on the mass
+     *
      * @param mass Particle's mass
      */
     void init_masses_and_energy(float mass);
 
     /**
      * @brief Reconstructs one particle per two particles in an event.
-     * 
+     *
      */
     void reconstruct_particles();
 };
